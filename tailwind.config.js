93899e67c6ff551fea3ccc00cfbfd33331c83c52
diegoangelgimenez-sys <diegoangelgimenez@gmail.com>
2025-11-12 23:38:28 +0100

    Fix: tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-   
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.{css}", 
+    "./**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aipcyan: "#00E0FF",
+        aipblue: "#006CFF",
+      },
+    },
   },
   plugins: [],
 };
